b9ae6a19937d2ccd9e31a6e9c702f34c2b4d02e6
ocmai <saki04mai@gmail.com>
2019-11-28 11:32:51 +0900

    Update

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+var date=[
+  {'id':1,'name':'水素','symbol':'H','hint1';,'hint2','hint3':},
+  {'id':2,'name':'ヘリウム','symbol':'He','hint1';,'hint2','hint3':},
+  {'id':3,'name':'リチウム','symbol':'Li','hint1';,'hint2','hint3':},
+  {'id':4,'name':'ベリリウム','symbol':'Be','hint1';,'hint2','hint3':},
+  {'id':5,'name':'ホウ素','symbol':'B','hint1';,'hint2','hint3':},
+  {'id':6,'name':'炭素','symbol':'C','hint1';,'hint2','hint3':},
+  {'id':7,'name':'窒素','symbol':'N','hint1';,'hint2','hint3':},
+  {'id':8,'name':'酸素','symbol':'O','hint1';,'hint2','hint3':},
+  {'id':9,'name':'フッ素','symbol':'F','hint1';,'hint2','hint3':},
+  {'id':10,'name':'ネオン','symbol':'Ne','hint1';,'hint2','hint3':},
+  {'id':11,'name':'ナトリウム','symbol':'Na','hint1';,'hint2','hint3':},
+  {'id':12,'name':'マグネシウム','symbol':'Mg','hint1';,'hint2','hint3':},
+  {'id':13,'name':'アルミニウム','symbol':'Al','hint1';,'hint2','hint3':},
+  {'id':14,'name':'ケイ素','symbol':'Si','hint1';,'hint2','hint3':},
+  {'id':15,'name':'リン','symbol':'P','hint1';,'hint2','hint3':},
+  {'id':16,'name':'硫黄','symbol':'S','hint1';,'hint2','hint3':},
+  {'id':17,'name':'塩素','symbol':'Cl','hint1';,'hint2','hint3':},
+  {'id':18,'name':'アルゴン','symbol':'Ar','hint1';,'hint2','hint3':},
+  {'id':19,'name':'カリウム','symbol':'K','hint1';,'hint2','hint3':},
+  {'id':20,'name':'カルシウム','symbol':'Ca','hint1';,'hint2','hint3':}
 
+]
 
 $(document).ready(function(){
     console.log('test');
